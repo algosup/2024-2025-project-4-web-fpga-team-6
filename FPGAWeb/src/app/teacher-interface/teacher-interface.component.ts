2746157dfb3125ddd5a643d3a97ede5258f39686
Benoît de Keyn <benoit.dekeyn@algosup.com>
2025-03-25 03:31:29 +0100

    clean some forsaken dependencies

--- FPGAWeb/src/app/teacher-interface/teacher-interface.component.ts
+++ FPGAWeb/src/app/teacher-interface/teacher-interface.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
